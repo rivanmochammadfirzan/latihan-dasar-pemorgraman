8e8026bb82ff6d7988f54ef5086ed1d8dd537ad6
rivanmochammadfirzan <114631390+rivanmochammadfirzan@users.noreply.github.com>
2022-09-29 11:27:59 +0700

    Add files via upload

---
+++ aplikasi biodata.py
@@ -0,0 +1,12 @@
+# bodata app made by rivan nochammad firzan
+
+# masukan nama
+nama = input("masukan nama anda! ")
+print("nama anda adaiah ", nama)
+nim = input("masukan nim anda! ")
+print("nim anda adalah", nim)
+kelas = input("masukan kelas anda! ")
+print("kelas anda adalah",kelas)
+prodi = input("masukan prodi anda! ")
+print("prodi anda adalah",prodi)
+
